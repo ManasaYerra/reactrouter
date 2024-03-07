bfbd2e4083ed408698ff02150b7356fabbff46ad
Manasa Y <144911557+ManasaYerra@users.noreply.github.com>
2024-03-07 19:21:40 +0530

    Created Components
    .Created components
    .Applied style sheets

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
